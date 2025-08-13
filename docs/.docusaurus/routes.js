bc84854885cf6e09cef3e4730c76092097191475
meis2m <meis2m@yahoo.com>
2025-08-13 23:21:17 +0330

    files

---
+++ docs/.docusaurus/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  ,
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
